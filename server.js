cb3bb4256a5cd4d63cb4013f03ec0fa4a075db8c
fpslexie <yts1337@gmail.com>
2026-05-13 09:07:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-let currentSong = "Nothing playing";
+let currentSong = "Ничего не играет";
 
-app.post("/scrobble", (req, res) => {
+app.post("/update", (req, res) => {
     const { artist, track } = req.body;
 
-    if (!track) return res.sendStatus(400);
-
-    currentSong = artist ? `${artist} - ${track}` : track;
-
-    console.log("🎵", currentSong);
+    if (artist && track) {
+        currentSong = `${artist} - ${track}`;
+        console.log("Updated:", currentSong);
+    }
 
     res.sendStatus(200);
 });
@@ -24,6 +20,8 @@ app.get("/song", (req, res) => {
     res.send(currentSong);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running");
 });
